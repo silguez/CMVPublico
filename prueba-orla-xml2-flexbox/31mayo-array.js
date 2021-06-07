7bfe6af2b40c433142978bdae8b4e50a0fbd5fc4
silguez <slvrr295@gmail.com>
2021-06-07 21:27:39 +0200

    modal en galeria

--- prueba-orla-xml2-flexbox/31mayo-array.js
+++ prueba-orla-xml2-flexbox/31mayo-array.js
@@ -10,7 +10,7 @@ function leerXML() {
     }
   };
   xhr.open("GET", "https://silguez.github.io/CMVPublico/datos.xml", true);
-  
+
   xhr.send();
 }
 function miFuncion(xml) {
@@ -19,22 +19,28 @@ function miFuncion(xml) {
   var foto;
   var elemento = [];
   var xmlDoc = xml.responseXML;
-  var contenedor="";
+  var contenedor = "";
   var y = xmlDoc.getElementsByTagName("elemento");
 
 
-  contenedor += "<div id='fotos'>";
+  contenedor += "<div class='container'><div class='card-columns' id='galeria'>";
   for (i = 0; i < y.length; i++) {
     // leo las etiquetas que me interesan del objeto
+    id = y[i].getElementsByTagName("id")[0].childNodes[0].nodeValue;
     nombre = y[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
     foto = y[i].getElementsByTagName("foto")[0].childNodes[0].nodeValue;
-   //actualizo contenido
-    contenedor += "<img class='eachPh' src='" + foto + "'  alt='" + nombre + "'/>";
+    //actualizo contenido
+    contenedor += `
+    <div class='card'>
+      <a href='#' data-toggle='modal' data-target='#id${id}'>
+        <img class='card-img' src= '${foto}' +  alt='${nombre}'/>
+      </a>
+    `
     // actualizo el array
     elemento = [foto, nombre];
     registrados.push(elemento);
   }
-  contenedor += "</div>"
+  contenedor += "</div></div>"
   document.getElementById("mensaje").innerHTML = contenedor;
 
   // muestro en consola el array
@@ -43,4 +49,32 @@ function miFuncion(xml) {
       console.log(datos);
     });
   });
+
+  var galeria = document.getElementById('galeria');
+
+  for (imagen of id) {
+    galeria.innerHTML += `
+  <div class="modal fade" id="#id${id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+  aria-hidden="true">
+
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+
+  <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+    <img src="${foto}" class="img-fluid rounded">
+  </div>
+
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+    </div>
+  </div>
+</div>
+</div>
+  `
+  }
+
 }
+
+
